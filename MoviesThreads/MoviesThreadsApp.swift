f976d0e6ba32cf38d0489d1cd87f242c0e639b39
cauanlgaldino <cauanlgaldino@gmail.com>
2025-05-13 21:31:25 -0300

    Initial Commit

---
+++ MoviesThreads/MoviesThreadsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesThreadsApp.swift
+//  MoviesThreads
+//
+//  Created by Cauan Lopes Galdino on 13/05/25.
+//
+
+import SwiftUI
+
+@main
+struct MoviesThreadsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
