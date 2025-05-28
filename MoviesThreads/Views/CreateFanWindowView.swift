c48f77ac4ed1b3a6102ed16371fb24fd1ce0af0b
cauanlgaldino <cauanlgaldino@gmail.com>
2025-05-28 11:21:15 -0300

    windowViews details

--- MoviesThreads/Views/CreateFanWindowView.swift
+++ MoviesThreads/Views/CreateFanWindowView.swift
@@ -4,21 +4,45 @@
 //
 //  Created by Júlia Saboya on 27/05/25.
 //
+
 import SwiftUI
 
 struct CreateFanWindowView: View {
     var body: some View {
-        VStack {
-            VStack(alignment: .leading) {
-                Text("Insira a capacidade máxima da sala")
-                TextField("Ex: 5", text: .constant(""))
-            }
+        Group {
+            VStack {
+                VStack(alignment: .leading) {
+                    Text("Nome do Fã")
+                        .font(.headline)
+                    TextField("Identificador", text: .constant(""))
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                }
+                .padding()
+                .background(.ultraThinMaterial)
+                .cornerRadius(10)
+                .shadow(radius: 5)
+                
 
-        VStack(alignment: .leading) {
-            Text("Insira a duração do filme em segundos")
-            TextField("Ex: 30", text: .constant(""))
+                VStack(alignment: .leading) {
+                    Text("Tempo de Lanche")
+                        .font(.headline)
+                    TextField("Segundos", text: .constant(""))
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                }
+                .padding()
+                .background(.ultraThinMaterial)
+                .cornerRadius(10)
+                .shadow(radius: 5)
+                
+                Button("Criar Fã"){
+                    
+                }
+                .buttonStyle(.borderedProminent)
+                .padding()
+            }
+            .padding(.vertical)
         }
-    }
+        
         .padding()
     }
 }
--- MoviesThreads/Views/GlobalWindowView.swift
+++ MoviesThreads/Views/GlobalWindowView.swift
@@ -4,21 +4,46 @@
 //
 //  Created by Júlia Saboya on 27/05/25.
 //
+
 import SwiftUI
 
 struct GlobalWindowView: View {
     var body: some View {
-        VStack {
-            VStack(alignment: .leading) {
-                Text("Insira a capacidade máxima da sala")
-                TextField("Ex: 5", text: .constant(""))
+        Group{
+            VStack {
+                VStack(alignment: .leading) {
+                    Text("Capacidade do Auditório")
+                        .font(.headline)
+                    TextField("Pessoas", text: .constant(""))
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                }
+                .padding()
+                .background(.ultraThinMaterial)
+                .cornerRadius(10)
+                .shadow(radius: 5)
+                .padding(.bottom, 10)
+                
+                
+                VStack(alignment: .leading) {
+                    Text("Duração do filme")
+                        .font(.headline)
+                    TextField("Segundos", text: .constant(""))
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                }
+                .padding()
+                .background(.ultraThinMaterial)
+                .cornerRadius(10)
+                .shadow(radius: 5)
+                
+                Button("Criar Sala"){
+                    
+                }
+                .buttonStyle(.borderedProminent)
+                .padding(.top)
+//                .padding(.vertical)
             }
-
-        VStack(alignment: .leading) {
-            Text("Insira a duração do filme em segundos")
-            TextField("Ex: 30", text: .constant(""))
+            .padding(.vertical, 4)
         }
-    }
         .padding()
     }
 }
