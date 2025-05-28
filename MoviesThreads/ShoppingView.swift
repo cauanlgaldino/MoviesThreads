06187e390c362e418ac12e2842e4962d757b8c50
Yane <yanesantos2001@hotmail.com>
2025-05-28 19:41:16 -0300

    assets e fonte

--- MoviesThreads/ShoppingView.swift
+++ MoviesThreads/ShoppingView.swift
@@ -9,17 +9,30 @@ import SwiftUI
 import Foundation
 
 struct ShoppingView: View {
+    @StateObject private var session = MovieSessionViewModel(capacity: 3, exhibitionTime: 30)
+    @State private var fanIDGenerator = 0
     var body: some View {
         GeometryReader { geometry in
             ZStack(alignment: .topLeading) {
-                Color.white
-                    .edgesIgnoringSafeArea(.all)
+                Image("Background")
+                    .resizable()
+                    .scaledToFill()
                 
                 ScreenView(size: geometry.size)
-                LogsView(size: geometry.size)
+                LogsView(size: geometry.size, logs: .constant(session.log))
                 DoorView(size: geometry.size)
                 ProjectorView(size: geometry.size)
                 SeatsView(size: geometry.size)
+                HStack {
+                    Button("➕ Criar Fã") {
+                        fanIDGenerator += 1
+                        let fan = Fan(id: "F\(fanIDGenerator)", session: session, snackTime: TimeInterval(Int.random(in: 10...20)))
+                        session.fans.append(fan)
+                        fan.start()
+                        
+                    }
+                    .buttonStyle(.borderedProminent)
+                }
             }
         }
     }
@@ -32,29 +45,56 @@ struct LayoutConstants {
     static let logHeightRatio: CGFloat = 0.19
     static let doorWidthRatio: CGFloat = 0.03
     static let doorHeightRatio: CGFloat = 0.2
-    static let projectorWidthRatio: CGFloat = 0.36
-    static let projectorHeightRatio: CGFloat = 0.29
-    static let seatWidthRatio: CGFloat = 0.088
-    static let seatHeightRatio: CGFloat = 0.127
+    static let projectorWidthRatio: CGFloat = 0.315
+    static let projectorHeightRatio: CGFloat = 0.255
+    static let seatWidthRatio: CGFloat = 0.09
+    static let seatHeightRatio: CGFloat = 0.14
+    static let theaterHeightRatio: CGFloat = 0.8
+    static let theaterWidthRatio: CGFloat = 0.55
 }
 
 struct LogsView: View {
     let size: CGSize
+    @Binding var logs: [LogEntry]
     
     var body: some View {
         HStack(spacing: 6) {
-            Rectangle()
-                .fill(Color.blue)
-            Rectangle()
-                .fill(Color.red)
-            Rectangle()
-                .fill(Color.gray)
+            Log(title: "Demonstrador", color: .greenDemonstrator, logs: logs)
+            Log(title: "Sala", color: .redTheater, logs: logs)
+            Log(title: "Fãs", color: .purpleFan, logs: logs)
         }
         .frame(width: size.width,
                height: size.height * LayoutConstants.logHeightRatio,
                alignment: .center)
         .position(x: size.width / 2,
                   y: size.height - size.height * LayoutConstants.logHeightRatio / 2)
+        .clipped()
+    }
+}
+
+struct Log: View {
+    let title: String
+    let color: Color
+    let logs: [LogEntry]
+    var body: some View {
+        ZStack(alignment: .topLeading) {
+            Image("DisplayLog")
+                .resizable()
+            VStack(alignment: .leading, spacing: 10) {
+                Text(title)
+                    .font(.custom("PixelifySans-Semibold", size: 32))
+                    .foregroundStyle(color)
+                VStack(spacing: 6) {
+                    ForEach(logs.suffix(4).reversed(), id: \.self) { log in
+                        Text(log.message)
+                            .font(.custom("PixelifySans-Semibold", size: 24))
+                            .foregroundStyle(.whiteLog)
+                    }
+                }
+                .clipped()
+            }
+            .padding(.leading, 50)
+        }
     }
 }
 
@@ -62,8 +102,8 @@ struct DoorView: View {
     let size: CGSize
     
     var body: some View {
-        Rectangle()
-            .fill(Color.brown)
+        Image("Door")
+            .resizable()
             .frame(width: size.width * LayoutConstants.doorWidthRatio,
                    height: size.height * LayoutConstants.doorHeightRatio)
             .position(
@@ -80,13 +120,13 @@ struct ProjectorView: View {
     let size: CGSize
     
     var body: some View {
-        Rectangle()
-            .fill(Color.cyan)
+        Image("Pele")
+            .resizable()
             .frame(width: size.width * LayoutConstants.projectorWidthRatio,
                    height: size.height * LayoutConstants.projectorHeightRatio)
             .position(x: size.width / 2,
                       y: size.height * LayoutConstants.screenHeightRatio / 2
-                        - size.height * LayoutConstants.projectorHeightRatio / 2)
+                      - size.height * LayoutConstants.projectorHeightRatio/1.5)
     }
 }
 
@@ -94,12 +134,18 @@ struct ScreenView: View {
     let size: CGSize
     
     var body: some View {
-        Rectangle()
-            .fill(Color.black)
-            .frame(width: size.width * LayoutConstants.screenWidthRatio,
-                   height: size.height * LayoutConstants.screenHeightRatio)
-            .position(x: size.width / 2,
-                      y: size.height * LayoutConstants.screenHeightRatio / 2)
+        ZStack {
+            Rectangle()
+                .fill(Color.theater)
+                .frame(width: size.width * LayoutConstants.screenWidthRatio,
+                       height: size.height * LayoutConstants.screenHeightRatio)
+            Image("MovieTheater")
+                .resizable()
+                .frame(width: size.width * LayoutConstants.theaterWidthRatio,
+                       height: size.height * LayoutConstants.theaterHeightRatio)
+        }
+        .position(x: size.width / 2,
+                  y: size.height * LayoutConstants.screenHeightRatio / 2)
     }
 }
 
@@ -108,9 +154,8 @@ struct SeatsView: View {
     
     var body: some View {
         ForEach(0..<10) { index in
-            Rectangle()
-                .fill(Color.green)
-                .border(Color.black)
+            Image("ChairBrown")
+                .resizable()
                 .frame(width: size.width * LayoutConstants.seatWidthRatio,
                        height: size.height * LayoutConstants.seatHeightRatio)
                 .position(
